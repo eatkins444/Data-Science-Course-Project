77bd7aadde573e447178ad5fbf064644dc58926d
eatkins444 <elvin.atkins.jr@gmail.com>
2021-02-15 13:06:10 -0500

    Create File on RStudio, Commit to GitHub

---
+++ Data-Science-Course-Project-File.R
@@ -0,0 +1,4 @@
+print("This file was created within RStudio for the Course Project")
+print("Demonstratin use of RStudio, Projects and version control with GitHub")
+Print("Save within File"
+      )
